c2fb0dbd951966ac3930b8f748dc0a53c5ce67cd
keumju <keumju0409@gmail.com>
2024-08-07 21:35:19 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import {Route, Routes, Link} from 'react-router-dom';
 import Header from './components/header/header'
 import Main from './components/main/main'
 
@@ -7,11 +6,8 @@ function App() {
 
   return (
     <div className="App">
-      <nav><Link to='/'></Link></nav>
       <Header/>
-      <Routes>
-        <Route path='/' element={<Main/>}></Route>
-      </Routes>
+      <Main/>
     </div>
   );
 }
